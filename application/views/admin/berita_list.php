cb5150bc028124f6b087347944c1d8c258604a31
Nengputrirmdn <ramdaninengputri@gmail.com>
2025-05-24 13:39:02 +0700

    tombol

--- application/views/admin/berita_list.php
+++ application/views/admin/berita_list.php
@@ -19,8 +19,8 @@
                 <td><?= $row->tanggal ?></td>
                 <td><?= $row->tag ?></td>
                 <td>
-                    <a href="<?= base_url('berita/detail/'.$row->id) ?>" class="btn btn-info btn-sm">Detail</a>
-                    <a href="<?= base_url('berita/edit/'.$row->id) ?>" class="btn btn-warning btn-sm">Edit</a>
+                    <a href="<?= base_url('berita/detail/'.$row->id) ?>" class="btn btn-info btn-sm me-1">Detail</a>
+                    <a href="<?= base_url('berita/edit/'.$row->id) ?>" class="btn btn-warning btn-sm me-1">Edit</a>
                     <a href="<?= base_url('berita/hapus/'.$row->id) ?>" class="btn btn-danger btn-sm" onclick="return confirm('Yakin hapus?')">Hapus</a>
                 </td>
             </tr>
