44560544cb6c092ed38ea435897cab62eb9480ca
ridwan2709 <ridwan.andriansyah73@gmail.com>
2025-05-24 12:03:52 +0700

    fix navbar

--- application/views/nav_web.php
+++ application/views/nav_web.php
@@ -2,7 +2,7 @@
     <div class="container-fluid p-0">
         <nav class="navbar navbar-expand-lg bg-dark navbar-dark py-2 py-lg-0 px-lg-5">
             <a href="index.html" class="navbar-brand d-block d-lg-none">
-                <h1 class="m-0 display-4 text-uppercase text-primary">Biz<span class="text-white font-weight-normal">News</span></h1>
+                <h1 class="m-0 display-4 text-uppercase text-primary">SMK<span class="text-white font-weight-normal">M-U</span></h1>
             </a>
             <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
                 <span class="navbar-toggler-icon"></span>
